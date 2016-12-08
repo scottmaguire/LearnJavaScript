0e28a838981ab8d1c0a6caa1228e449ed6ca5b63
scott <scottmaguirerfc@hotmail.com>
2016-12-08 11:54:59 +0000

    first test

--- tests/appSpec.js
+++ tests/appSpec.js
@@ -1,3 +1,15 @@
 /**
  * Created by davem on 27/11/2016.
  */
+describe("learn javascript", function(){
+    it("cas show question view",function(){
+        learnjavascript.showView("$question-1");
+        expect($(".view-container .question-view").length).toEqual(1);
+      });
+
+    it("shows the landing page view when their is not  hash", function(){
+
+    
+
+    })
+    });
